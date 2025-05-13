490ce4d0c422dd6901688d3230d36e137847be9c
Fryansb <ryan.sbv2@gmail.com>
2025-05-13 20:40:42 -0300

    Impede recarregamento do formulário e adiciona script de envio via JS

---
+++ assets/main.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('form-indicacao');
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const dados = new FormData(form);
+    const nome = dados.get('nome');
+    const email = dados.get('email');
+    const livro = dados.get('livro');
+    const observacao = dados.get('observacao');
+
+    console.log({ nome, email, livro, observacao });
+    alert('Sugestão enviada com sucesso!');
+    form.reset();
+  });
+});
